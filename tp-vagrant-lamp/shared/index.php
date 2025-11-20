1676c81178667642e1a4d8291fa8e1a42d90238e
james-pc-perso <james@pc-perso.fr>
2025-11-20 20:18:19 +0100

    tp-lamp 2.1.3 ajustement sur index.php et rm .html

--- tp-vagrant-lamp/shared/index.php
+++ tp-vagrant-lamp/shared/index.php
@@ -1,19 +1,1 @@
-<?php
-$servername = "192.168.56.11";
-$username = "tp_user";
-$password = "tp_password";
-$dbname = "tp_db";
 
-// Créer une connexion MySQLi
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Vérifier la connexion
-if ($conn->connect_error) {
-    echo "Connexion à la base de données échouée : " . $conn->connect_error;
-} else {
-    echo "Connexion à la base de données réussie !";
-}
-
-// Fermer la connexion
-$conn->close();
-?>
