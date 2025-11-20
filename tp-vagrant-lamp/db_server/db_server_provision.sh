c7a24f011eab3dc15ea7df5e7fa22db57cb58326
james-pc-perso <james@pc-perso.fr>
2025-11-20 19:40:58 +0100

    tp-lamp 2.1.2 j'ai oublier mariadb-client... -_-'

--- tp-vagrant-lamp/db_server/db_server_provision.sh
+++ tp-vagrant-lamp/db_server/db_server_provision.sh
@@ -2,7 +2,7 @@
 
 # Mettre à jour et installer MariaDB
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y mariadb-server
+DEBIAN_FRONTEND=noninteractive apt-get install -y mariadb-server mariadb-client
 
 # Configurer MariaDB pour écouter toutes les interfaces
 sed -i "s/^bind-address\s*=.*/bind-address = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
